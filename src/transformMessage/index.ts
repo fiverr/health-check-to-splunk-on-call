b4b0acf375f6a39c96a4fc0520e98a7b73baa38c
omrilotan <516342+omrilotan@users.noreply.github.com>
2022-05-26 17:32:30 +0100

    Prefix alert name with "Cloudflare Health Check"
    ```
    monitoring_tool: API
    - entity_display_name: P0-Sub-Categories-Page
    + entity_display_name: Cloudflare Health Check P0-Sub-Categories-Page 
    state_message: Resolved by shmuel_yomtov
    ACKED_AT: 1653579381918
    ACKED_BY: haim_misparaim
    ```

--- src/transformMessage/index.ts
+++ src/transformMessage/index.ts
@@ -3,10 +3,10 @@ import { IncomingPayload, OutgoingPayload } from "../types";
 
 export const transformMessage = ({
 	text,
-	data: { health_check_id: entity_id, name: entity_display_name, status },
+	data: { health_check_id: entity_id, name, status },
 	ts: state_start_time,
 }: IncomingPayload): OutgoingPayload => ({
-	entity_display_name,
+	entity_display_name: `Cloudflare Health Check ${name}`,
 	entity_id,
 	message_type: statusDictionary(status),
 	state_message: text.trim(),
